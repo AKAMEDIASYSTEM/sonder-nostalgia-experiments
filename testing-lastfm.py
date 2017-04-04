08985446d5ed309714f4cb086517144ce73a095f
AKA <nfeehan@gmail.com>
2017-04-03 23:05:48 -0400

    now trying to add a most-recent loc to each song

--- testing-lastfm.py
+++ testing-lastfm.py
@@ -53,6 +53,8 @@ for u in db.locations.find().skip(3):
 '''
 
 for ss in db.songs.find({"time":{"$gt":0}}).limit(3):
+        songtime = ss['time']
+        print 'songtime'
         locresult = db.locations.find({"time":{"$gt":ss['time'],"$lte":ss['time']}}).sort([("time",1)]).limit(1)
 	print 'hey now'
         print locresult
