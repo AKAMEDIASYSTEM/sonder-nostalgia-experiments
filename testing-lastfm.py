282d93a5b8601371eb93f89ea582f57c4e6ca7c2
AKA <nfeehan@gmail.com>
2017-04-08 15:28:25 -0400

    seems like lastfm has wacky timestamps too, normalizing now

--- testing-lastfm.py
+++ testing-lastfm.py
@@ -56,7 +56,9 @@ for u in db.songs.find({"time":{"$gt":0}}).skip(3):
         timeraw = int(u['time'] / 1000.0)
         ti = datetime.fromtimestamp(timeraw)
         idd = u['_id']
-        print db.songs.update({"_id":idd},{"$set":{"time":ti}})
+        db.songs.update({"_id":idd},{"$set":{"time":ti}})
+print 'done with timestamp update'
+print db.songs.findOne({"time":{"$gt":0}})
 
 '''
 for ss in db.songs.find({"time":{"$gt":0}}).skip(3).limit(3):
