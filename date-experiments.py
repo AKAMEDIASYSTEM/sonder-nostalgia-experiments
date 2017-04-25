e60cf7208a796a3755fab4ca8ee3bdefdaf0201b
AKA <nfeehan@gmail.com>
2017-04-25 14:55:44 -0400

    trying to group and reconcile dates, just messin right now

--- date-experiments.py
+++ date-experiments.py
@@ -18,7 +18,7 @@ for u in db.songs.find({"timestamp":{"$gt":datetime.fromtimestamp(1180639052)}})
         date1 = u['timestamp'] - timedelta(days=0.5)
         date2 = u['timestamp'] + timedelta(days=0.5)
         r = db.locations.find({"time":{"$gte":date1, "$lte":date2}}).sort("time")
-        print '%i matches at %s'% (r, u['timestamp'])
+        # print '%i matches at %s'% (r, u['timestamp'])
         for result in r:
                 print 'hey now, match at %s' % u['timestamp']
                 print result
