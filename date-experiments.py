da6a6e10063e75dae001c41e542ac8ffd7027988
AKA <nfeehan@gmail.com>
2017-04-25 14:56:18 -0400

    trying to group and reconcile dates, just messin right now

--- date-experiments.py
+++ date-experiments.py
@@ -14,7 +14,7 @@ from pymongo import MongoClient
 client = MongoClient()
 db = client.nostalgia
 
-for u in db.songs.find({"timestamp":{"$gt":datetime.fromtimestamp(1180639052)}}).limit(4):
+for u in db.songs.find({"timestamp":{"$gt":datetime.fromtimestamp(1180639052)}}).limit(40):
         date1 = u['timestamp'] - timedelta(days=0.5)
         date2 = u['timestamp'] + timedelta(days=0.5)
         r = db.locations.find({"time":{"$gte":date1, "$lte":date2}})
