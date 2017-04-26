36e5aaec181bc85251e72ef89de8d1af1c3ebd54
AKA <nfeehan@gmail.com>
2017-04-26 14:01:46 -0400

    starting work on reconciler

---
+++ reconciler.py
@@ -0,0 +1,14 @@
+
+from __future__ import division
+import json
+import pprint
+import random
+import math
+import sys
+from datetime import datetime
+from datetime import timedelta
+from geojson import Point
+import pymongo
+from pymongo import MongoClient
+import spotipy
+import akacreds
