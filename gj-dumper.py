c09566153a23bc40f38cf23e734ef1dd9924f996
AKA <nfeehan@gmail.com>
2017-04-27 15:19:03 -0400

    quick sidetrack to vis one syncwalk set of points

---
+++ gj-dumper.py
@@ -0,0 +1,24 @@
+
+from __future__ import division
+import json
+import geojson
+import pprint
+import random
+import math
+import sys
+from datetime import datetime
+from datetime import timedelta
+from geojson import Point
+import pymongo
+from pymongo import MongoClient
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import akacreds
+import os
+
+
+client = MongoClient()
+db = client.nostalgia
+
+r = db.syncwalks.find({}).limit(1)
+print r['gj']
